45a59ad8802d6cca827d1c204d3d380a4eab729a
Abhishek Rana <abhishekrana4a0@gmail.com>
2025-06-16 22:15:22 +0530

    nodejs day-1

--- Web_Dev/Backend/l1/app.js
+++ Web_Dev/Backend/l1/app.js
@@ -1,3 +1,21 @@
-let a=10;
-let b=20;
-console.log(a+b);
+// let a=10;
+// let b=20;
+// console.log(a+b);
+
+// function statement
+function greet(){
+    console.log("greet.");
+}
+greet();
+
+//function are first class
+function logGreeting(fn){
+    fn();
+}
+logGreeting(greet);
+
+// function expression
+const greet2 = function(){
+    console.log("greet2.");
+}
+greet2();
